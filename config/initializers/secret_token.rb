b8666b95a979c991d24a71bb4fb12cf522541705
Ben Pollock <B.O.Pollock@gmail.com>
2013-10-19 22:41:27 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EZ4X::Application.config.secret_key_base = '9e7a30571231c4ce225d54eeec23ba7ee9551031823269a70cd6e3df96fd26cc638c2af3d82f7e2aaf7e043a73a472ddaa816aca1a6e79cc01a989c5eb302e6a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EZ4X::Application.config.session_store :cookie_store, key: '_EZ4X_session'
