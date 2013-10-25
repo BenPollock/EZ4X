87cff234bd14a2efc677850adb33f5d0f1580a58
Ben Pollock <B.O.Pollock@gmail.com>
2013-10-24 21:47:36 -0400

    Updated demo chart to more accurate EUR/USD data

--- app/assets/javascripts/trader.js
+++ app/assets/javascripts/trader.js
@@ -5,7 +5,7 @@ $(function() {
 			useUTC : false
 		}
 	});
-	
+	var EUR = 13793;
 	// Create the chart
 	$('#container').highcharts('StockChart', {
 		chart : {
@@ -15,22 +15,37 @@ $(function() {
 					// set up the updating of the chart each second
 					var series = this.series[0];
 					var flags = this.series[1];
+					var low_in_a_row = 0;
+					var up_in_a_row = 0;
 					setInterval(function() {
-						var a = (new Date()).getTime(), // current time
-						y = Math.round(Math.random() * 100);
+						var a = (new Date()).getTime(); // current time
+
+						var change = Math.floor(Math.random() * 3) - 1;
+						y = EUR + change;
+						EUR = y;
+
+						if(change < 0){
+							low_in_a_row++;
+							up_in_a_row = 0;
+						}else if (change > 0){
+							up_in_a_row++;
+							low_in_a_row = 0;
+						}
+						
 						series.addPoint([a, y], true, true);
-						if(y < 20){
+						if(low_in_a_row > 2){
 							flags.addPoint({
 								title: "Buy",
 								x: a
 							});
 							buy();
 						}
-						if(y > 80){
+						if(up_in_a_row > 2){
 							flags.addPoint({
 								title: "Sell",
 								x: a
 							});
+							sell();
 						}
 					}, 1000);
 				}
@@ -55,7 +70,7 @@ $(function() {
 		},
 		
 		title : {
-			text : 'Live random data'
+			text : 'EUR/USD Fake Data'
 		},
 		
 		exporting: {
@@ -71,8 +86,10 @@ $(function() {
 				for( i = -999; i <= 0; i++) {
 					data.push([
 						time + i * 1000,
-						Math.round(Math.random() * 100)
+						//Math.round(Math.random() * 100)
+						EUR
 					]);
+					EUR += Math.floor(Math.random() * 3) - 1;
 				}
 				return data;
 			})(),
@@ -90,4 +107,20 @@ $(function() {
 
 function buy(){
 	//alert("buy");
+}
+
+function sell(){
+
+}
+
+function calcBollinger(){
+
+}
+
+function calcRSI(){
+
+}
+
+function calcMACD(){
+
 }
