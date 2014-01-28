ad3f9890174650da471d6d779d3742240980c601
Ben Pollock <B.O.Pollock@gmail.com>
2014-01-28 13:41:41 -0500

    Fixed migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131020034025) do
+ActiveRecord::Schema.define(version: 20140128183030) do
 
   create_table "models", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -32,6 +32,20 @@ ActiveRecord::Schema.define(version: 20131020034025) do
   add_index "models", ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
 
   create_table "quotes", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "sessions", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "trade_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "trades", force: true do |t|
+    t.integer  "quote_id"
+    t.integer  "units"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
