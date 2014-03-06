36066f7c40ac5fb06eed14fcf52aeed87964fcb3
Ben Pollock <B.O.Pollock@gmail.com>
2014-03-05 20:16:01 -0500

    reset db

--- db/migrate/20140304222024_remove_trade_id_from_session.rb
+++ db/migrate/20140304222024_remove_trade_id_from_session.rb
@@ -1,6 +1,6 @@
 class RemoveTradeIdFromSession < ActiveRecord::Migration
   def change
     remove_column :sessions, :trade_id, :string
-    #remove_column :sessions, :integer, :string
+    remove_column :sessions, :integer, :string
   end
 end
--- db/migrate/20140304222059_remove_quote_id_from_trade.rb
+++ db/migrate/20140304222059_remove_quote_id_from_trade.rb
@@ -1,6 +1,6 @@
 class RemoveQuoteIdFromTrade < ActiveRecord::Migration
   def change
     remove_column :trades, :quote_id, :string
-   # remove_column :trades, :integer, :string
+    remove_column :trades, :integer, :string
   end
 end
