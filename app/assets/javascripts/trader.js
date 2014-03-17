2ff344d23d09f553c82a50b82f5ffe9f5f96de43
Ben Pollock <B.O.Pollock@gmail.com>
2014-03-17 15:48:11 -0400

    precomiple

--- app/assets/javascripts/trader.js
+++ app/assets/javascripts/trader.js
@@ -402,25 +402,6 @@ $(function() {
             yAxis: 1,
             showInLegend: true,
             type: 'histogram'
-        },
-        {
-        	name : 'RSI',
-			/*data : (function() {
-			// generate an array of random data
-			var data = [], time = (new Date()).getTime(), i;
-
-			for( i = -999; i <= 0; i++) {
-				data.push([
-					time + i * 1000,
-					//Math.round(Math.random() * 100)
-					50
-				]);
-			}
-			return data;
-			})(),*/
-			yAxis: 2,
-			id: 'rsi',
-			showInLegend: true
         }]
 	});
 
